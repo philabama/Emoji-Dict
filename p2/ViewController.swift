f43a1dc0e2030efb62a586e160a03f05b540ec7b
Scott Smith <scottsmac@MacBook-Pro.local>
2017-06-14 15:11:53 -0500

    no message

--- p2/ViewController.swift
+++ p2/ViewController.swift
@@ -3,13 +3,28 @@
 //  p2
 //
 //  Created by Scott Smith on 6/14/17.
-//  Copyright © 2017 Scott Smith. All rights reserved.
+//  Copyright © 2017 Scott Smith. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var theLabel: UILabel!
+
+    var tapCnt = 0
+    
+    @IBAction func hteBtn(_ sender: Any) {
+        theLabel.text = "YO"
+        print("yo Console!")
+        
+        tapCnt = tapCnt + 1
+        print (tapCnt)
+        
+        if tapCnt >= 10{
+        theLabel.text = "You tapped 10 times!"
+        }}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
